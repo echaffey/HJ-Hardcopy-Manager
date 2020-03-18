fdf42564c325dbd41b3baa397e24fb6a6b22025b
Evan Chaffey <46167224+echaffey@users.noreply.github.com>
2020-03-18 15:37:38 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
+import extract_1
+#https://stackoverflow.com/questions/26494211/extracting-text-from-a-pdf-file-using-pdfminer-in-python
 
 
 def pdf_splitter(path):
@@ -9,16 +11,20 @@ def pdf_splitter(path):
     pdf = PdfFileReader(path)
 
     #iterate through the pages and pull them out into their own separate PDF
-    for page in range(pdf.getNumPages()):
+    for page in range(2):#pdf.getNumPages()):
         pdf_writer = PdfFileWriter()
         pdf_writer.addPage(pdf.getPage(page))
+
         #Save files to a separate output folder and add a counter to separate them
+        
         '''
         Insert text parser here
           - pull out the P&O, WJ and State
           - create the output filename as 'PO_State'
         '''
 
+        text = extract_1.convert_pdf_to_txt(pdf.getPage(page))
+        print(text)
         #final will be = 'output/{PO_num}_{State_name}.pdf'
         output_filename = 'output/{}_page_{}.pdf'.format(fname, page)
 
@@ -29,5 +35,4 @@ def pdf_splitter(path):
 
 if __name__ == '__main__':
     path = 'test.pdf'
-    print(os.path.splitext(os.path.basename(path)))
-    # pdf_splitter(path)
+    pdf_splitter(path)
--- extract_1.py
+++ extract_1.py
@@ -6,21 +6,21 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
 
-def convert_pdf_to_txt(path):
+def convert_pdf_to_txt(page):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
     codec = 'utf-8'
     laparams = LAParams()
     device = TextConverter(rsrcmgr, retstr, laparams=laparams)
-    fp = open(path, 'rb')
+    #fp = open(path, 'rb')
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     password = ""
     maxpages = 10
     caching = True
     pagenos=set()
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
+    #for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+    interpreter.process_page(page)
 
     text = retstr.getvalue()
 
