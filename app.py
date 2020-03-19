f0a9b87dc3d10ec918d0f970504af8403dcd9197
Evan Chaffey <echaffey@my.uri.edu>
2020-03-18 23:24:04 -0400

    Implemented file upload

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, flash, request, redirect, render_template, url_for, send_from_directory
 from werkzeug.utils import secure_filename
+import pdf_functions as pf
 import os
 
 
-UPLOAD_FOLDER = '/static/uploads'
+UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = ['application/pdf', 'pdf']
 
 app = Flask(__name__)
@@ -11,7 +12,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'HerffJones19'
 
 def allowed_file(filename):
-    print('.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/uploads/<filename>')
@@ -22,9 +22,9 @@ def uploaded_file(filename):
 def index():
     if request.method == 'POST':
         # check if the post request has the file part
-        # if 'file' not in request.files:
-        #     flash('File not found')
-        #     return redirect(request.url)
+        if 'file' not in request.files:
+            flash('File not found')
+            return redirect(request.url)
         file = request.files['file']
 
         # if user does not select file, browser also
@@ -36,18 +36,19 @@ def index():
         #if a correct file is uploaded
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
+
+            #Save the file to the static/uploads folder
             file.save(f'{app.config["UPLOAD_FOLDER"]}/{filename}')
+
+            #Split the file and collect the text
+            pf.pdf_splitter(f'{app.config["UPLOAD_FOLDER"]}/{filename}')
+
+            #Delete the file once it's been split and parsed
+            os.remove(f'static/uploads/{filename}')
+            
             # return redirect(url_for('uploaded_file',
             #                         filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pdf_functions.py
+++ pdf_functions.py
@@ -1,8 +1,14 @@
-import os
-from PyPDF2 import PdfFileReader, PdfFileWriter
-import extract
-import emailer
+#pip install pdfminer.six
+#pip install PyPDF2
 #https://stackoverflow.com/questions/26494211/extracting-text-from-a-pdf-file-using-pdfminer-in-python
+import os
+import emailer
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
 
 
 def pdf_splitter(path):
@@ -30,7 +36,7 @@ def pdf_splitter(path):
             pdf_writer.write(out)
 
         #Raw text read from PDFs, split into a list for easier retrieval
-        text = extract.convert_pdf_to_txt(output_filename)
+        text = convert_pdf_to_txt(output_filename)
         text_list = text.split('\n')
 
         #Remove excess empty string values
@@ -54,6 +60,36 @@ def pdf_splitter(path):
         #rename the file to inclue the PO, State and page counter (to remove duplication issues)
         os.rename(output_filename, f'output/{PO_num}_{state}_{page}.pdf' )
 
+def convert_pdf_to_txt(path):
+    """
+    Parses the text from a PDF at the given path.
+    path: filepath of the PDF to parse.
+    return: str
+    """
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 10
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    #Make sure everything is closed as to not run into file issues
+    fp.close()
+    device.close()
+    retstr.close()
+
+    #Returns PDF text as string
+    return text
 
 if __name__ == '__main__':
     path = 'test.pdf'
