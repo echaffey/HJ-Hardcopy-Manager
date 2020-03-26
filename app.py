c4e530b2087d775fa193b2aa385c7981b90de3b3
Evan Chaffey <echaffey@my.uri.edu>
2020-03-25 21:11:45 -0400

    code cleanup

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, flash, request, redirect, render_template, url_for, send_from_directory
+from flask import Flask, request, redirect, render_template, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import pdf_functions as pf
 import os
 import sys
 
 #Directory to temporarily store uploaded file
-UPLOAD_FOLDER = os.path.join('static', 'uploads')
+UPLOAD_FOLDER      = os.path.join('static', 'uploads')
 ALLOWED_EXTENSIONS = ['application/pdf', 'pdf']
+alert_messages     = []
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SECRET_KEY'] = 'HerffJones19'
+app.config['SECRET_KEY']    = 'HerffJones19'
 
 def allowed_file(filename):
     """
@@ -19,19 +20,30 @@ def allowed_file(filename):
     """
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def alert(message, category):
+    """
+    Display pop up message alerts.  Stored in reverse order for most recent message on top.
+    """
+    global alert_messages
+    if len(alert_messages) > 4: alert_messages.pop()
+    alert_messages = [(message, category)] + alert_messages
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    Index page that the flask app initially loads to.
+    """
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('File not found', 'error')
+            alert('File not found.', 'danger')
             return redirect(request.url)
         file = request.files['file']
 
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file', 'warning')
+            alert('No file selected.', 'warning')
             return redirect(request.url)
 
         #if a correct file is uploaded
@@ -43,15 +55,15 @@ def index():
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
             #Split the file and collect the text
-            pf.pdf_splitter(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            pf.split_PDF(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
             #Delete the file once it's been split and parsed
             os.remove(os.path.join('static', 'uploads', filename))
-            flash(f'Uploaded {file.filename} successfully', 'success')
+            alert(f'Uploaded {file.filename} successfully.', 'success')
             
-            return render_template('index.html')
+            return render_template('index.html', alerts=alert_messages)
 
-    return render_template('index.html')
+    return render_template('index.html', alerts=alert_messages)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pdf_functions.py
+++ pdf_functions.py
@@ -4,7 +4,6 @@
 
 import os
 import re
-import pdf_parser
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
@@ -15,21 +14,24 @@ import datetime
 import sys
 
 
-# SAVE_FOLDER = r'F:\\USERS\\PUBLIC\\Hardcopy Storage\\CSR Hardcopies'
-SAVE_FOLDER = os.path.join('output')
+DEVELOPMENT = True
 
-def pdf_splitter(path):
+if DEVELOPMENT:
+    SAVE_FOLDER = os.path.join('output')
+else:
+    SAVE_FOLDER = r'F:\\USERS\\PUBLIC\\Hardcopy Storage\\CSR Hardcopies'
+
+def split_PDF(path):
     """
     Method to split a multi-page PDF into single page files.
-    This breaks up the Oracle file into individual hard copies.
+    This breaks up the Oracle file into individual hardcopies.
 
     path: filepath of the Oracle multi-page PDF
     """
 
     #get pdf name without the file extension and load pdf file
-    fname = os.path.splitext(os.path.basename(path))[0]
-    #load pdf file
-    pdf = PdfFileReader(path)
+    fname     = os.path.splitext(os.path.basename(path))[0]
+    pdf       = PdfFileReader(path)
     TIMESTAMP = datetime.datetime.now().strftime("%m%d_")
 
     #iterate through the pages and pull them out into their own separate PDF
@@ -37,53 +39,51 @@ def pdf_splitter(path):
         pdf_writer = PdfFileWriter()
         pdf_writer.addPage(pdf.getPage(page))
 
-        #just temporary output name to navigate only being able to pull text with a filename
+        #Create a temporary output name to create the file with
+        #before parsing and renaming it later
         output_filename = f'output/{fname}_page_{page}.pdf'
 
         #Save files to a separate output folder and add a counter to separate them
-        #try:
-        counter = 0
-        #If the file doesnt exist already, create it. 
-        if not os.path.exists(output_filename):
-            with open(output_filename, 'wb') as out:
-                pdf_writer.write(out)
-        else:
-            #Account for if theres the same zip and same state scanned at the same time
-            while os.path.exists(os.path.join(SAVE_FOLDER, f'delete_{fname}_{TIMESTAMP}{counter}.pdf')):
-                counter += 1
-                output_filename = os.path.join(SAVE_FOLDER, f'delete_{fname}_{TIMESTAMP}{counter}.pdf')
-            with open(output_filename, 'wb') as out:
-                pdf_writer.write(out)
-    
-        #Raw text read in from PDFs, split into a list for easier retrieval
-        text = convert_pdf_to_txt(output_filename)
+        try:
+            counter = 0
+            #If the file doesnt exist already, create it. 
+            if not os.path.exists(output_filename):
+                with open(output_filename, 'wb') as out:
+                    pdf_writer.write(out)
+            else:
+                #Account for if theres the same zip and same state scanned at the same time
+                while os.path.exists(os.path.join(SAVE_FOLDER, f'delete_{fname}_{TIMESTAMP}{counter}.pdf')):
+                    counter += 1
+                    output_filename = os.path.join(SAVE_FOLDER, f'delete_{fname}_{TIMESTAMP}{counter}.pdf')
+                with open(output_filename, 'wb') as out:
+                    pdf_writer.write(out)
+        
+            #Raw text read in from PDFs, split into a list for easier retrieval
+            text   = convert_pdf_to_txt(output_filename)
 
-        #extract all data
-        pdf_parser.parse(text)
+            #Find the 'P&O' numbers
+            PO_num = str(re.search(r'(P&O:\s*)(\d{7})', text).group(2))
 
-        #Find the 'P&O' numbers
-        PO_num = str(re.search(r'(P&O:\s*)(\d{7})', text).group(2))
+            #Find the state initials
+            state  = str(re.search(r'(\d{7}\s)([A-Z]{2})', text).group(2))
 
-        #Find the state initials
-        state = str(re.search(r'(\d{7}\s)([A-Z]{2})', text).group(2))
+            #rename the file to inclue the PO, State and page counter (to remove duplication issues)
+            new_filename = os.path.join(SAVE_FOLDER, f'{PO_num}_{state}_{TIMESTAMP}.pdf')
 
-        #rename the file to inclue the PO, State and page counter (to remove duplication issues)
-        new_filename = os.path.join(SAVE_FOLDER, f'{PO_num}_{state}_{TIMESTAMP}.pdf')
+            #if the file doesnt exist, rename the temp filename to the new one
+            if not os.path.exists(new_filename):
+                os.rename(output_filename, new_filename)
 
-        #if the file doesnt exist, rename the temp filename to the new one
-        if not os.path.exists(new_filename):
-            os.rename(output_filename, new_filename)
+            else:
+                #If it already exists, append a counter number to the end to not remove duplicates
+                while os.path.exists(os.path.join(SAVE_FOLDER, f'{PO_num}_{state}_{TIMESTAMP}{counter}.pdf')):
+                    counter += 1
 
-        else:
-            #If it already exists, append a counter number to the end to not remove duplicates
-            while os.path.exists(os.path.join(SAVE_FOLDER, f'{PO_num}_{state}_{TIMESTAMP}{counter}.pdf')):
-                counter += 1
-
-            new_filename = os.path.join(SAVE_FOLDER, f'{PO_num}_{state}_{TIMESTAMP}{counter}.pdf')
-            # os.remove(output_filename)
-            os.rename(output_filename, new_filename)
-        # except:
-        #     pass
+                new_filename = os.path.join(SAVE_FOLDER, f'{PO_num}_{state}_{TIMESTAMP}{counter}.pdf')
+                # os.remove(output_filename)
+                os.rename(output_filename, new_filename)
+        except:
+            print('------ Something happened here, but let\'s just pretend it didn\'t ------')
 
 def convert_pdf_to_txt(path):
     """
@@ -91,17 +91,17 @@ def convert_pdf_to_txt(path):
     path: filepath of the PDF to parse.
     return: str
     """
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
-    fp = open(path, 'rb')
+    rsrcmgr     = PDFResourceManager()
+    retstr      = StringIO()
+    codec       = 'utf-8'
+    laparams    = LAParams()
+    device      = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp          = open(path, 'rb')
     interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 10
-    caching = True
-    pagenos=set()
+    password    = ""
+    maxpages    = 10
+    caching     = True
+    pagenos     = set()
 
     for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
         interpreter.process_page(page)
@@ -115,7 +115,3 @@ def convert_pdf_to_txt(path):
 
     #Returns PDF text as string
     return text
-
-if __name__ == '__main__':
-    path = 'test.pdf'
-    pdf_splitter(path)
